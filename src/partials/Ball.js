6710b54958476fe837b275577eb63228f74ed0b7
Kelvin Loh <dotcal604@me.com>
2018-12-05 19:11:21 -0800

    Add wallCollision function to Ball class.

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -8,6 +8,12 @@ export default class Ball {
         this.boardHeight = boardHeight;
         this.direction = 1;
         this.reset();
+        document.addEventListener('keydown', event => {
+            console.log(event);
+            if(event.key == ' ') {
+                this.reset();
+            }
+        })
         }
     
     reset() {
@@ -20,9 +26,24 @@ export default class Ball {
         this.vx = this.direction * (6 - Math.abs(this.vy));
     }
 
+    wallCollision() {
+        const hitLeft = (this.x - this.radius <= 0);
+        const hitRight = (this.x + this.radius >= this.boardWidth);
+        const hitTop = (this.y - this.radius <= 0);
+        const hitBottom = (this.y + this.radius >= this.boardHeight);
+        if (hitTop || hitBottom) {
+            this.vy *= -1;
+            this.vx *= -1;
+        } else if (hitLeft || hitRight) {
+            this.vy *= -1;
+            this.vx *= -1;
+        }
+    }
+    
+
     render(svg) {
-        this.x = this.x + this.vx;
-        this.y = this.y + this.vy;
+        this.x += this.vx;
+        this.y += this.vy;
         let circle = document.createElementNS(SVG_NS, 'circle');
         
 		circle.setAttributeNS(null, 'r', this.radius);
@@ -31,6 +52,7 @@ export default class Ball {
         circle.setAttributeNS(null, 'fill', 'white');
         
         svg.appendChild(circle);
+        this.wallCollision();
         this.x = this.x + this.vx;
         this.y = this.y + this.vy;
     }
