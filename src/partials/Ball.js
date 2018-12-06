6da7e5a397062a3152e20616f6bde0b73ecc3d33
Kelvin Loh <dotcal604@me.com>
2018-12-05 18:49:50 -0800

    Add logic for Ball class.

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -6,16 +6,23 @@ export default class Ball {
         this.radius = radius;
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
+        this.direction = 1;
         this.reset();
-    }
-
+        }
+    
     reset() {
         this.x = this.boardWidth / 2;
         this.y = this.boardHeight / 2;
+        this.vy = 0;
+        while (this.vy === 0) {
+            this.vy = Math.floor(Math.random() * 10 - 5);
+        }
+        this.vx = this.direction * (6 - Math.abs(this.vy));
     }
 
     render(svg) {
-		
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
         let circle = document.createElementNS(SVG_NS, 'circle');
         
 		circle.setAttributeNS(null, 'r', this.radius);
@@ -24,6 +31,7 @@ export default class Ball {
         circle.setAttributeNS(null, 'fill', 'white');
         
         svg.appendChild(circle);
-
-	}
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
+    }
 }
